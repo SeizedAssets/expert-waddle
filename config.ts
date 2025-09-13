487210aeff55a241d0b0e1d4c8df9db2a2cc1b19
Seized User <164658008+SeizedAssets@users.noreply.github.com>
2025-09-13 04:34:53 -0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 // SERVER DETAILS
-export const domain = 'google.com';
+export const domain = 'expert-waddle.seizedassets.deno.net';
 export const fullDomain = `https://${domain}`;
 export const proxies: string[] = [];
 export const fakeQr = true;
